6069a50de3f8954b307671bf2a2a8053078eb583
its julii <nanji828@gmail.com>
2022-12-04 19:14:49 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _04._Sum_of_Two_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startInterval = int.Parse(Console.ReadLine());
+            int endInterval = int.Parse(Console.ReadLine());
+            int magicNumber = int.Parse(Console.ReadLine());
+            int counter = 0;
+            int validCombination = 0;
+
+            for (int leftNumber = startInterval; leftNumber <= endInterval; leftNumber++)
+            {
+                for (int rightNumber = startInterval; rightNumber <= endInterval; rightNumber++)
+                {
+                    counter++;
+                    int sum = leftNumber + rightNumber;
+                    if (sum == magicNumber)
+                    {
+                        validCombination++;
+
+                        Console.WriteLine($"Combination N:{counter} ({leftNumber} + {rightNumber} = {sum})");
+
+                        break;
+                    }
+
+                }
+                if (validCombination == 1)
+                {
+                    break;
+                }
+            }
+            if (validCombination != 1)
+            {
+                Console.WriteLine($"{counter} combinations - neither equals {magicNumber}");
+            }
+        }
+    }
+}
